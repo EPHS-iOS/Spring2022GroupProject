acf7fe25b6a920685777f975a7517a9c9d998254
Jennifer Nelson <jnelson@edenpr.org>
2022-04-14 10:15:47 -0500

    initial files

---
+++ PostItApp/PostItAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostItAppApp.swift
+//  PostItApp
+//
+//  Created by Jennifer Nelson on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PostItAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
