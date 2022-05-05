ba5e493642a0a4b1eb9f4af620392e87eb35bd72
Lucas Wagner <lwagner.mn@gmail.com>
2022-05-05 15:03:35 -0500

    Update OnboardingScreen.swift

--- OnboardingScreen.swift
+++ OnboardingScreen.swift
@@ -1,30 +1,53 @@
-//
-//  LoadingSquareView.swift
-//  ScavengeItAnimations
-//
-//  Created by Lucas Wagner on 4/18/22.
-//
-
-import SwiftUI
-import Lottie
-
-struct OnboardingScreen : UIViewRepresentable {
-    typealias UIViewType = UIView
-    func makeUIView(context: UIViewRepresentableContext<OnboardingScreen>) -> UIView {
-        let view = UIView(frame: .zero)
-        let animationView = AnimationView()
-        let animation = Animation.named("PostItAnimation")
-        animationView.animation = animation
-        animationView.contentMode = .scaleAspectFit
-        animationView.play()
-        animationView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(animationView)
-        NSLayoutConstraint.activate([animationView.widthAnchor.constraint(equalTo: view.widthAnchor), animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
-                                    ])
-        return view
+struct animationScreen: View{
+   
+    // Will need some padding
+  fileprivate struct square: View{
+    fileprivate var color: Color
+    var body: some View{
+      Rectangle().aspectRatio(1, contentMode: .fit).frame(width: 100, height: 100, alignment: .center).foregroundColor(color).overlay{
+        Rectangle().stroke(lineWidth: 2).aspectRatio(1, contentMode: .fit).frame(width: 100, height: 100, alignment: .center).foregroundColor(Color.black)
+      }
     }
-    
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<OnboardingScreen>) {
+  }
+   
+  @State fileprivate var rotation: Double
+  @State fileprivate var rotToAchieve: Double
+  fileprivate var x: Int
+  fileprivate var y: Int
+   
+  fileprivate var timings: [Double]
+   
+ 
+  init(animationTimings t: [Double] = [1.5, 1.8, 2.0], offsetX x: Int = 20, offsetY y: Int = 25, originalRotation r: Double = 45, rotationToAchieve rt: Double = 180+45){
+     
+    self.timings = t
+    self.x = x
+    self.y = y
+    self.rotation = r
+    self.rotToAchieve = rt
+     
+  }
+  var body: some View{
+    ZStack{
+      square(color: Color.black)
+        .rotationEffect(.degrees(rotation), anchor: .center)
+        .animation(Animation.easeInOut(duration: timings[0]), value: rotation)
+      ZStack{
+        square(color: Color.white)
+          .rotationEffect(.degrees(rotation), anchor: .center)
+          .animation(Animation.easeInOut(duration: timings[1]), value: rotation)
+      }.offset(x: CGFloat(x), y: CGFloat(y))
+      ZStack{
+        square(color: Color(hexString: "c94848"))
+          .rotationEffect(.degrees(rotation), anchor: .center)
+          .animation(Animation.easeInOut(duration: timings[2]), value: rotation)
+      }.offset(x: CGFloat(x*2), y: CGFloat(y*2))
+
+    }.onAppear{
+      DispatchQueue.main.async {
+        rotation = rotToAchieve
+      }
+       
     }
+  }
 }
-
